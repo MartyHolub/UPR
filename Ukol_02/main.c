78d4fe4d8448ca317c93b25374d9456a9c206c31
Martin Holub <82341655+MartyHolub@users.noreply.github.com>
2024-10-03 21:36:05 +0200

    first 2 tasks

--- Ukol_02/main.c
+++ Ukol_02/main.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(){
+    int obrazec = 0;
+    int a = 0, b = 0;
+    printf("Zadejte hodnotu obrazce a parametry A B: \n");
+    scanf("%d %d %d", &obrazec, &a, &b);
+
+    switch(obrazec){
+        case 1: {
+            for(int i = 1, i < a, i++){
+                for(int j = 1, j < b, j++){
+                    printf("x");
+                }
+                printf("\n");
+            }
+            break;
+        }
+        case 2: {
+            for(int i = 1, i < a, i++){
+                for(int j = 1, j < b, j++){
+                    if((i = 1 || i = a-1) || (j = 1 || j = b-1) )printf("x");
+                }
+                printf("\n");
+            }
+        }
+        case 3: {
+            
+        }
+        case 4: {
+            
+        }
+        case 5: {
+            
+        }
+        case 6: {
+            
+        }
+        case 7: {
+            
+        }
+    }
+
     for(int i = 0;i<10;i+2){
         printf("%d ",i);
     }
-    
+    return 0;
 }
